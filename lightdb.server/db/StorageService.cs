2da781e1c6eb9f6d6cdcaca4f3c4f6d3ca5bb9a0
lights <lightsever@hotmail.com>
2018-11-20 21:42:17 +0800

    add server

---
+++ lightdb.server/db/StorageService.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LightDB;
+namespace lightdb.server
+{
+
+    public class StorageService
+    {
+        public static readonly byte[] tableID_Writer = new byte[] { 0x07 };
+        public LightDB.LightDB maindb;
+        public bool state_DBOpen
+        {
+            get;
+            private set;
+        }
+
+        public void Init()
+        {
+            Console.CursorLeft = 0;
+            Console.WriteLine(" == Open DB ==");
+
+
+            maindb = new LightDB.LightDB();
+
+            state_DBOpen = false;
+            string fullpath = System.IO.Path.GetFullPath(Program.config.server_storage_path);
+            if (System.IO.Directory.Exists(fullpath) == false)
+                System.IO.Directory.CreateDirectory(fullpath);
+            string pathDB = System.IO.Path.Combine(fullpath, "maindb");
+            try
+            {
+                maindb.Open(pathDB);
+                state_DBOpen = true;
+                Console.WriteLine("db opened in:" + pathDB);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("error msg:" + err.Message);
+            }
+            if (state_DBOpen == false)
+            {
+                Console.WriteLine("open database fail. try to create it.");
+                try
+                {
+                    LightDB.DBCreateOption createop = new LightDB.DBCreateOption();
+                    createop.MagicStr = Program.config.storage_maindb_magic;
+                    createop.FirstTask = new LightDB.WriteTask();
+
+                    createop.FirstTask.CreateTable(new LightDB.TableInfo(tableID_Writer, "_writeraddress_", "", LightDB.DBValue.Type.String));
+                    createop.FirstTask.Put(tableID_Writer, Program.config.storage_maindb_firstwriter_address.ToBytes_UTF8Encode(), LightDB.DBValue.FromValue(LightDB.DBValue.Type.BOOL, true));
+
+                    maindb.Open(pathDB, createop);
+                    Console.WriteLine("db created in:" + pathDB);
+                    state_DBOpen = true;
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine("error msg:" + err.Message);
+
+                }
+            }
+        }
+
+    }
+}
---
+++ lightdb.server/net/websocketpeer.cs
@@ -0,0 +1,66 @@
+﻿using lightdb.sdk;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lightdb.server
+{
+
+    public class websockerPeer : lightchain.httpserver.httpserver.IWebSocketPeer
+    {
+        System.Net.WebSockets.WebSocket websocket;
+        
+        public websockerPeer(System.Net.WebSockets.WebSocket websocket)
+        {
+            this.websocket = websocket;
+        }
+        public async Task OnConnect()
+        {
+            Console.CursorLeft = 0;
+
+            Console.WriteLine("websocket in:" + websocket.SubProtocol);
+        }
+
+        public async Task OnDisConnect()
+        {
+            Console.CursorLeft = 0;
+
+            Console.WriteLine("websocket gone:" + websocket.CloseStatus + "." + websocket.CloseStatusDescription);
+
+        }
+        private async Task SendToClient(NetMessage msg)
+        {
+            await this.websocket.SendAsync(msg.ToBytes(), System.Net.WebSockets.WebSocketMessageType.Binary, true, System.Threading.CancellationToken.None);
+        }
+        public async Task OnRecv(System.IO.MemoryStream stream, int recvcount)
+        {
+            var p = stream.Position;
+            var msg = NetMessage.Unpack(stream);
+            var pend = stream.Position;
+            if (pend - p > recvcount)
+                throw new Exception("error net message.");
+
+            var iddata = msg.Params["_id"];
+            if (iddata.Length != 8)
+                throw new Exception("error net message _id");
+
+            switch (msg.Cmd)
+            {
+                case "_ping":
+                    await OnPing(msg, iddata);
+                    break;
+                default:
+                    throw new Exception("unknown msg cmd:" + msg.Cmd);
+            }
+        }
+        public async Task OnPing(NetMessage msgRecv,byte[] id)
+        {
+            var msg = NetMessage.Create("_pingback");
+            msg.Params["_id"] = id;
+
+            await SendToClient(msg);
+
+        }
+    }
+}
