8f9acd70beed6b1560efde2b1723d9af9165401f
lights <lightsever@hotmail.com>
2018-11-21 22:29:42 +0800

    编写了基础协议代码

--- lightdb.server/net/websocketpeer.cs
+++ lightdb.server/net/websocketpeer.cs
@@ -14,6 +14,8 @@ namespace lightdb.server
         public websockerPeer(System.Net.WebSockets.WebSocket websocket)
         {
             this.websocket = websocket;
+            this.peerSnapshots = new System.Collections.Concurrent.ConcurrentDictionary<ulong, ISnapShot>();
+            this.peerItertors = new System.Collections.Concurrent.ConcurrentDictionary<ulong, IEnumerator<byte[]>>();
         }
         public async Task OnConnect()
         {
@@ -27,7 +29,33 @@ namespace lightdb.server
             Console.CursorLeft = 0;
 
             Console.WriteLine("websocket gone:" + websocket.CloseStatus + "." + websocket.CloseStatusDescription);
+            DisposeSnapShots();
+        }
+        public void DisposeSnapShots()
+        {
+            try
+            {
+                foreach (var psnap in peerSnapshots.Values)
+                {
+                    if (psnap != null)
+                    {
+                        try
+                        {
+                            psnap.Dispose();
+                        }
+                        catch
+                        {
 
+                        }
+                    }
+                }
+
+                peerSnapshots.Values.Clear();
+            }
+            catch
+            {
+
+            }
         }
         private async Task SendToClient(NetMessage msg)
         {
@@ -99,8 +127,7 @@ namespace lightdb.server
         {
             var msg = NetMessage.Create("_ping.back");
             msg.Params["_id"] = id;
-
-            await SendToClient(msg);
+            SendToClient(msg);
 
         }
         public async Task OnDB_State(NetMessage msgRecv, byte[] id)
@@ -110,113 +137,271 @@ namespace lightdb.server
             msg.Params["dbopen"] = new byte[] { (byte)(Program.storage.state_DBOpen ? 1 : 0) };
             if (Program.storage.state_DBOpen)
             {
-                using (var snap = Program.storage.maindb.UseSnapShot())
+                try
                 {
-                    msg.Params["height"] = BitConverter.GetBytes(snap.DataHeight);
-                    //Console.WriteLine("dataheight=" + snap.DataHeight);
-                    var value = snap.GetValue(LightDB.LightDB.systemtable_info, "_magic_".ToBytes_UTF8Encode());
-                    if (value != null)
-                        msg.Params["magic"] = value.value;
-
-                    var keys = snap.CreateKeyFinder(StorageService.tableID_Writer);
-                    var i = 0;
-                    foreach (byte[] keybin in keys)
+                    using (var snap = Program.storage.maindb.UseSnapShot())
                     {
-                        //var key = keybin.ToString_UTF8Decode();
-                        var iswriter = snap.GetValue(StorageService.tableID_Writer, keybin).AsBool();
-                        if (iswriter)
+                        msg.Params["height"] = BitConverter.GetBytes(snap.DataHeight);
+                        //Console.WriteLine("dataheight=" + snap.DataHeight);
+                        var value = snap.GetValue(LightDB.LightDB.systemtable_info, "_magic_".ToBytes_UTF8Encode());
+                        if (value != null)
+                            msg.Params["magic"] = value.value;
+
+                        var keys = snap.CreateKeyFinder(StorageService.tableID_Writer);
+                        var i = 0;
+                        foreach (byte[] keybin in keys)
                         {
-                            i++;
-                            msg.Params["writer" + i] = keybin;
-                            //Console.WriteLine("writer:" + key);
+                            //var key = keybin.ToString_UTF8Decode();
+                            var iswriter = snap.GetValue(StorageService.tableID_Writer, keybin).AsBool();
+                            if (iswriter)
+                            {
+                                i++;
+                                msg.Params["writer" + i] = keybin;
+                                //Console.WriteLine("writer:" + key);
+                            }
                         }
                     }
                 }
+                catch (Exception err)
+                {
+                    msg.Params["_error"] = err.Message.ToBytes_UTF8Encode();
+                }
             }
-            await SendToClient(msg);
+            else
+            {
+                msg.Params["_error"] = "db not open.".ToBytes_UTF8Encode();
+            }
+            SendToClient(msg);
 
         }
+
+        //收集所有snapshot
+        public System.Collections.Concurrent.ConcurrentDictionary<UInt64, ISnapShot> peerSnapshots;
+        public System.Collections.Concurrent.ConcurrentDictionary<UInt64, IEnumerator<byte[]>> peerItertors;
         public async Task OnDB_UseSnapShot(NetMessage msgRecv, byte[] id)
         {
             var msg = NetMessage.Create("_db.usesnapshot.back");
             msg.Params["_id"] = id;
-            msg.Params["_error"] = "not implement yet".ToBytes_UTF8Encode();
-            await SendToClient(msg);
+            try
+            {
+                UInt64? wantheight = msg.Params.ContainsKey("snapheight") ? (UInt64?)BitConverter.ToUInt64(msg.Params["snapheight"], 0) : null;
+                if (wantheight == null)
+                {
+                    var snapshot = Program.storage.maindb.UseSnapShot();
+                    wantheight = snapshot.DataHeight;
+                    peerSnapshots[snapshot.DataHeight] = snapshot;
+                }
+                else
+                {
+                    if (peerSnapshots.ContainsKey(wantheight.Value) == false)
+                    {
+                        msg.Params["_error"] = "do not have that snapheight".ToBytes_UTF8Encode();
+                    }
+                }
+                if (msg.Params.ContainsKey("_error") == false)
+                {
+                    msg.Params["snapheight"] = BitConverter.GetBytes(wantheight.Value);
+                }
+            }
+            catch (Exception err)
+            {
+                msg.Params["_error"] = err.Message.ToBytes_UTF8Encode();
+            }
+            SendToClient(msg);
         }
         public async Task OnDB_UnuseSnapShot(NetMessage msgRecv, byte[] id)
         {
             var msg = NetMessage.Create("_db.unusesnapshot.back");
             msg.Params["_id"] = id;
-            msg.Params["_error"] = "not implement yet".ToBytes_UTF8Encode();
-            await SendToClient(msg);
+            try
+            {
+                UInt64 snapheight = BitConverter.ToUInt64(msg.Params["snapheight"]);
+                bool b = peerSnapshots.TryRemove(snapheight, out ISnapShot value);
+                if (b)
+                {
+                    value.Dispose();
+                }
+                msg.Params["remove"] = new byte[] { (byte)(b ? 1 : 0) };
+            }
+            catch (Exception err)
+            {
+                msg.Params["_error"] = err.Message.ToBytes_UTF8Encode();
+            }
+            SendToClient(msg);
         }
         public async Task OnSnapshotDataHeight(NetMessage msgRecv, byte[] id)
         {
             var msg = NetMessage.Create("_db.snapshot.dataheight.back");
             msg.Params["_id"] = id;
-            msg.Params["_error"] = "not implement yet".ToBytes_UTF8Encode();
-            await SendToClient(msg);
+            try
+            {
+                UInt64 snapheight = BitConverter.ToUInt64(msg.Params["snapheight"]);
+                var snap = peerSnapshots[snapheight];
+                msg.Params["dataheight"] = BitConverter.GetBytes(snap.DataHeight);
+
+            }
+            catch (Exception err)
+            {
+                msg.Params["_error"] = err.Message.ToBytes_UTF8Encode();
+            }
+            SendToClient(msg);
         }
         public async Task OnSnapshot_GetValue(NetMessage msgRecv, byte[] id)
         {
             var msg = NetMessage.Create("_db.snapshot.getvalue.back");
             msg.Params["_id"] = id;
-            msg.Params["_error"] = "not implement yet".ToBytes_UTF8Encode();
-            await SendToClient(msg);
+            try
+            {
+                UInt64 snapheight = BitConverter.ToUInt64(msg.Params["snapheight"]);
+                var snap = peerSnapshots[snapheight];
+                byte[] data = snap.GetValueData(msg.Params["tableid"], msg.Params["key"]);
+                msg.Params["data"] = data;
+            }
+            catch (Exception err)
+            {
+                msg.Params["_error"] = err.Message.ToBytes_UTF8Encode();
+            }
+            //这个完全可以不要等待呀
+            SendToClient(msg);
         }
         public async Task OnSnapshot_GetTableCount(NetMessage msgRecv, byte[] id)
         {
             var msg = NetMessage.Create("_db.snapshot.gettablecount.back");
             msg.Params["_id"] = id;
-            msg.Params["_error"] = "not implement yet".ToBytes_UTF8Encode();
-            await SendToClient(msg);
+            try
+            {
+                UInt64 snapheight = BitConverter.ToUInt64(msg.Params["snapheight"]);
+                var snap = peerSnapshots[snapheight];
+                byte[] data = BitConverter.GetBytes(snap.GetTableCount(msg.Params["tableid"]));
+                msg.Params["count"] = data;
+            }
+            catch (Exception err)
+            {
+                msg.Params["_error"] = err.Message.ToBytes_UTF8Encode();
+            }
+            //这个完全可以不要等待呀
+            SendToClient(msg);
         }
         public async Task OnSnapshot_GetTableInfo(NetMessage msgRecv, byte[] id)
         {
             var msg = NetMessage.Create("_db.snapshot.gettableinfo.back");
             msg.Params["_id"] = id;
-            msg.Params["_error"] = "not implement yet".ToBytes_UTF8Encode();
-            await SendToClient(msg);
+            try
+            {
+                UInt64 snapheight = BitConverter.ToUInt64(msg.Params["snapheight"]);
+                var snap = peerSnapshots[snapheight];
+
+                //此处可以优化，增加一个GetTableInfoData,不要转一次
+                byte[] data = snap.GetTableInfo(msg.Params["tableid"]).ToBytes();
+                msg.Params["info"] = data;
+            }
+            catch (Exception err)
+            {
+                msg.Params["_error"] = err.Message.ToBytes_UTF8Encode();
+            }
+            //这个完全可以不要等待呀
+            SendToClient(msg);
         }
         public async Task OnSnapshot_CreateKeyIterator(NetMessage msgRecv, byte[] id)
         {
             var msg = NetMessage.Create("__db.snapshot.newiterator.back");
             msg.Params["_id"] = id;
-            msg.Params["_error"] = "not implement yet".ToBytes_UTF8Encode();
-            if (Program.storage.state_DBOpen)
+            try
             {
-                var snapshot = Program.storage.maindb.UseSnapShot();
-                snapshot.CreateKeyIterator(msg.Params["tableid"], null, null);
+                UInt64 snapheight = BitConverter.ToUInt64(msg.Params["snapheight"]);
+                var snap = peerSnapshots[snapheight];
+
+                //这里缺一个给唯一ID的方式,采用顺序编号是个临时方法
+                var beginkey = msg.Params?["beginkey"];
+                var endkey = msg.Params?["endkey"];
+                var iter = snap.CreateKeyIterator(msg.Params["tableid"], beginkey, endkey);
+                ulong index = (UInt64)this.peerItertors.Count;
+                this.peerItertors[index] = iter;
+
+                msg.Params["iteratorid"] = BitConverter.GetBytes(index);
             }
-            await SendToClient(msg);
+            catch (Exception err)
+            {
+                msg.Params["_error"] = err.Message.ToBytes_UTF8Encode();
+            }
+            //这个完全可以不要等待呀
+            SendToClient(msg);
+
         }
         public async Task OnSnapshot_IteratorCurrent(NetMessage msgRecv, byte[] id)
         {
             var msg = NetMessage.Create("_db.iterator.current.back");
             msg.Params["_id"] = id;
-            msg.Params["_error"] = "not implement yet".ToBytes_UTF8Encode();
-            await SendToClient(msg);
+            try
+            {
+                UInt64 iteratorid = BitConverter.ToUInt64(msg.Params["iteratorid"]);
+                var it = peerItertors[iteratorid];
+
+                var data = it.Current;
+                msg.Params["data"] = data;
+            }
+            catch (Exception err)
+            {
+                msg.Params["_error"] = err.Message.ToBytes_UTF8Encode();
+            }
+            //这个完全可以不要等待呀
+            SendToClient(msg);
         }
         public async Task OnSnapshot_IteratorNext(NetMessage msgRecv, byte[] id)
         {
-            var msg = NetMessage.Create("_db.iterator.next.back");
+            var msg = NetMessage.Create("_db.iterator.current.back");
             msg.Params["_id"] = id;
-            msg.Params["_error"] = "not implement yet".ToBytes_UTF8Encode();
-            await SendToClient(msg);
+
+            try
+            {
+                UInt64 iteratorid = BitConverter.ToUInt64(msg.Params["iteratorid"]);
+                var it = peerItertors[iteratorid];
+
+                var b = it.MoveNext();
+                msg.Params["movenext"] = new byte[] { (byte)(b ? 1 : 0) };
+            }
+            catch (Exception err)
+            {
+                msg.Params["_error"] = err.Message.ToBytes_UTF8Encode();
+            }
+            //这个完全可以不要等待呀
+            SendToClient(msg);
         }
         public async Task OnSnapshot_IteratorReset(NetMessage msgRecv, byte[] id)
         {
             var msg = NetMessage.Create("_db.iterator.reset.back");
             msg.Params["_id"] = id;
-            msg.Params["_error"] = "not implement yet".ToBytes_UTF8Encode();
-            await SendToClient(msg);
+            try
+            {
+                UInt64 iteratorid = BitConverter.ToUInt64(msg.Params["iteratorid"]);
+                var it = peerItertors[iteratorid];
+
+                it.Reset();
+            }
+            catch (Exception err)
+            {
+                msg.Params["_error"] = err.Message.ToBytes_UTF8Encode();
+            }
+            //这个完全可以不要等待呀
+            SendToClient(msg);
         }
         public async Task OnDB_Write(NetMessage msgRecv, byte[] id)
         {
             var msg = NetMessage.Create("_db.write.back");
             msg.Params["_id"] = id;
             msg.Params["_error"] = "not implement yet".ToBytes_UTF8Encode();
-            await SendToClient(msg);
+            try
+            {
+                var data = msg.Params["writetask"];
+                var writetask = WriteTask.FromRaw(data);
+                Program.storage.maindb.Write(writetask);
+            }
+            catch (Exception err)
+            {
+                msg.Params["_error"] = err.Message.ToBytes_UTF8Encode();
+            }
+            //这个完全可以不要等待呀
+            SendToClient(msg);
         }
     }
 }
